923c2e47037ab00f44f7e159220281ee8dd14dae
Brahmasura <ashlincds@gmail.com>
2024-04-20 04:20:36 +0530

    the typewriter effect applied to hero

--- src/Context/LinkContext/LinkContext.jsx
+++ src/Context/LinkContext/LinkContext.jsx
@@ -1,11 +1,13 @@
 // the linkContext
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export const LinkContext = createContext();
 
 export const LinkProvider = ({children}) => {
   const [activeLink, setActiveLink] = useState(0);
+  const location = useLocation();
 
   console.log("the page reloaded again");
 
@@ -14,6 +16,27 @@ export const LinkProvider = ({children}) => {
     setActiveLink(index);
     console.log(`the link index no ${index} has been clicked `);
   }
+
+  useEffect(() => {
+    // Set activeLink based on current route pathname
+    switch (location.pathname) {
+      case "/":
+        setActiveLink(0);
+        break;
+      case "/portfolio":
+        setActiveLink(1);
+        break;
+      case "/about":
+        setActiveLink(2);
+        break;
+      case "/contact":
+        setActiveLink(3);
+        break;
+      default:
+        setActiveLink(0);
+        break;
+    }
+  }, [location.pathname]);
 
   const value = {
     activeLink,
--- src/routes/Home/HeroSection/Hero.jsx
+++ src/routes/Home/HeroSection/Hero.jsx
@@ -1,12 +1,72 @@
+import gsap from "gsap";
 import style from "./hero.module.scss";
+import { TextPlugin } from "gsap/all";
+import { useRef } from "react";
+import { useEffect } from "react";
+
+// Register the TextPlugin with GSAP
+gsap.registerPlugin(TextPlugin);
 
 const Hero = () => {
+  const textRef = useRef(null);
+  const cursorRef = useRef(null);
+
+  useEffect(() => {
+    // const words = ["Boldness", "Clarity", "Originality", "Precision"];
+    const words = ["Nurturing Words", "harvesting Clarity", "Cultivating Quality"];
+        {/* <h3>Nurturing words, harvesting clarity, cultivating quality.</h3> */}
+
+    let mainTimeline = gsap.timeline({
+      repeat: -1,
+    });
+
+
+    // creating text timeline
+    words.forEach((word) => {
+      let textTimeline = gsap.timeline({
+        repeat: 1,
+        yoyo: true,
+        repeatDelay:6
+      });
+
+      textTimeline.to(textRef.current, {
+        text: word,
+        duration: 1,
+      });
+
+      mainTimeline.add(textTimeline);
+    });
+
+
+    // for blinking cursor effect 
+
+    let cursorTimeline = gsap.timeline({
+      repeat:-1,
+      repeatDelay:.8
+    });
+
+    cursorTimeline.to(cursorRef.current, {
+      opacity:1,
+      duration:0
+    } ).to(cursorRef.current, {
+      opacity:0,
+      duration:0,
+      delay:.8
+    });
+  }, []);
+
   return (
     <div className={style.heroMain}>
-        <h1>Edito Farm</h1>
-        <h3>Nurturing words, harvesting clarity, cultivating quality.</h3>
+      <div className={style.textDiv}>
+        <h1>
+          Edito Farm <br />
+          <span ref={textRef} className={style.typewriter}></span>
+          <span ref={cursorRef} className={style.cursor}>|</span>
+        </h1>
+        {/* <h3>Nurturing words, harvesting clarity, cultivating quality.</h3> */}
       </div>
-  )
-}
+    </div>
+  );
+};
 
 export default Hero;
