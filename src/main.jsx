330d99133055c9b27ba0d93b8e89d207a196dc42
Brahmasura <ashlincds@gmail.com>
2024-05-19 20:30:16 +0530

    basename adjusted

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { LinkProvider } from "./Context/LinkContext/LinkContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/editofarm">
+    <BrowserRouter basename="/projectFarm">
       <LinkProvider>
         <App />
       </LinkProvider>
